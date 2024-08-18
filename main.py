d7d2aec1b85df5e2832b126b7a1761c33360ee2c
Laura <laura.spellman.carey@gmail.com>
2024-08-17 21:14:00 -0600

    Trying to appease the linter :)

--- main.py
+++ main.py
@@ -7,23 +7,21 @@ state's capitol city
 Makes use of Docker & Polars
 
 '''
-import requests
 import json
 import logging
+import requests
 import polars as pl
-from api_secrets import OPENWEATHER_API_KEY, geocoding_url, current_weather_url
+from api_secrets import OPENWEATHER_API_KEY, GEOCODING_URL, CURRENT_WEATHER_URL
 
 def main():
-
     # setup dict to hold the temps to be averaged
     temps = {}
-
     # Get valid argument: a letter from user 
-    letter = input(f'Please enter the first letter of the state for which you would like the average temp: ')
+    letter = input(f'Please enter the first letter of the state for which you would \
+    like the average temp: ')
     while not(len(letter) == 1 and letter.isalpha()):
         letter = input("Invalid input. Please enter a single letter: ")
     print(f"Selecting states that begin with the letter {letter.upper()}")
-
     # Get states that begin with given letter
     df = pl.read_json('us_states.json',
                       schema={ 
@@ -33,54 +31,59 @@ def main():
                         }
                         )
     df = df.filter(pl.col('state').str.starts_with(f'{letter.upper()}'))
-
     # two queries are required to get the current weather: 
-    # 1 - pass the name of city, state, and country to the geocoder api -> lat, lon returned
+    # 1 - pass the name of city, state, and country to the 
+    #     geocoder api -> lat, lon returned
     # 2 - given lat, lon -> current weather data returned
     for row in df.rows():
-        print('***********************************************************************************')
+        print('************************************************************')
         city = row[2]
         state = row[1]
-
         # Query geocoder api to get from city name -> lat/lon
         qparams = f'?q={city},{state},US&appid={OPENWEATHER_API_KEY}'
-        code, jsonresp = call_api(geocoding_url, qparams)
+        code, jsonresp = call_api(GEOCODING_URL, qparams)
         if code == 200:
             try:
                 lat = jsonresp[0]['lat']
                 lon = jsonresp[0]['lon']
             except IndexError as e:
-                logging.warning(f'Due to request error in geocoder, Skipping {city}, {state}. Response: {jsonresp}')
+                logging.warning(f'Due to request error in geocoder, Skipping {city}, \
+                {state}. Response: {jsonresp}')
                 break 
         else:
-            logging.warning(f'Due to request error in geocoder, Skipping {city}, {state}. Response: {jsonresp}')
+            logging.warning(f'Due to request error in geocoder, Skipping {city}, \
+            {state}. Response: {jsonresp}')
             break
-
         # Query forecast api with lat/lon to get temps
         qparams = f'?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}'
-        code, jsonresp = call_api(current_weather_url, qparams)
-        
+        code, jsonresp = call_api(CURRENT_WEATHER_URL, qparams)
         # pull average weather out of response
         if code == 200:
            temps[f'{city}, {state}'] = jsonresp['main']['temp']
         else:
-            logging.warning(f'Due to request error in weather url, Skipping {city}, {state}. Response:  {jsonresp}')
+            logging.warning(f'Due to request error in weather url, Skipping \
+            {city}, {state}. Response:  {jsonresp}')
             break 
-
     # find average and display
-    print(f"The following temps are listed for state's capitals that begin with: {letter.upper()}")
+    print(f"The following temps are listed for state's capitals that begin \
+    with: {letter.upper()}")
     for city in temps:
         print(f'{city}: {temps[city]} Kelvin')
     if len(temps) ==0:
         print('no temps to average!')
         print('Bye!')
+        return
     avg = avg_temp(temps.values())
     print(f'The average temperature of the current temperatures: {avg} Kelvin')
     print('bye!')
+    return
 
        
 def avg_temp(temps: list) -> float:
-    return sum(temps)/len(temps)
+    if len(temps) > 0:
+        return sum(temps)/len(temps)
+    else:
+        return None
 
 
 def call_api(url: str, params: str) -> tuple[int, object]:
